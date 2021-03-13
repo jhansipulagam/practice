93fbd3f61305106f131a49d3643fb702772282b9
jhansipulagam <pulagamjhansi@gmail.com>
2021-03-12 23:45:32 -0800

    firstcommit

---
+++ ArithemeticOperatorsDemo.java
@@ -0,0 +1,18 @@
+public class ArithemeticOperatorsDemo{
+public static void main(String args[]){
+int a=13;
+int b=5;
+int sum=a+b;
+int sub=a-b;
+int mul=a*b;
+int div=a/b;
+double div1=a/b;
+int mod=a%b;
+System.out.println(sum);
+System.out.println(sub);
+System.out.println(mul);
+System.out.println(div);
+System.out.println(div1);
+System.out.println(mod);
+}
+}

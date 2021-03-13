60fc607d4229b01e8b0adb17b8cc14e012ebe349
jhansipulagam <pulagamjhansi@gmail.com>
2021-03-13 00:01:05 -0800

    secondcommit

---
+++ OneDimensionalArrayDemo.java
@@ -0,0 +1,8 @@
+public class OneDimensionalArrayDemo{
+public static void main(String args[]){
+int marks[]={20,30,40,50,60};
+for(int index=0;index<5;index++){
+System.out.println(marks [index]);
+}
+}
+}

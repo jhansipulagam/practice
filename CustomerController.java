bfc46f0e673b9e3113ecd0b9ddebebfa2d04766a
jhansipulagam <pulagamjhansi@gmail.com>
2021-03-20 02:23:28 -0700

    fisrtcommit

---
+++ CustomerController.java
@@ -0,0 +1,6 @@
+public class CustomerController{
+public static void main(String args[]){
+//This is about customer controller
+System.out.println("Hello Customer Controller");
+}
+}
--- OneDimensionalArrayDemo.java
+++ OneDimensionalArrayDemo.java
@@ -2,7 +2,7 @@ public class OneDimensionalArrayDemo{
 public static void main(String args[]){
 int marks[]={20,30,40,50,60};
 for(int index=0;index<5;index++){
-sSystem.out.println(marks [index]);
+system.out.println(marks [index]);
 }
 }
 }

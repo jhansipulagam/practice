e8ae1c5d9818ac2f0d68d59e882387fc560df7d3
jhansipulagam <pulagamjhansi@gmail.com>
2021-03-20 03:15:17 -0700

    multiline

---
+++ MultilineCommentDemo.java
@@ -0,0 +1,7 @@
+public class MultilineCommentDemo{
+public static void main(String args[]){
+/*iam jhansi and iam from vijayawadaand it is  a single line comment demo
+*/
+System.out.println("Multi line Comment Demo");
+}
+}

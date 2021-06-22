643ef170109cb97522fcc6728620686c6413dd94
jhansipulagam <pulagamjhansi@gmail.com>
2021-06-22 17:43:20 +0700

    nowcommit

---
+++ CardControllerApone.java
@@ -0,0 +1,9 @@
+public class CardControllerApone{
+public static void main(String args[]){
+Card c1=new Card();
+System.out.println(c1.cardId);
+System.out.println(c1.cardNumber);
+System.out.println(c1.expiryDate);
+System.out.println(c1.cardType);
+}
+}
